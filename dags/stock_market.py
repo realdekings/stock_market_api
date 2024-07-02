846ac63e038738636ec1091940b60680c84fc2ba
Kingsley <kingsley.ihemere@vet-ct.com>
2024-07-02 12:26:48 +0100

    implemented fetching of stock price from yahoo finance

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,9 +1,13 @@
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
 from datetime import datetime
 import requests
+from include.stock_market.tasks import _get_stock_prices
 
+
+SYMBOL = 'AAPL'
 
 @dag(
     start_date=datetime(2024, 1, 1),
@@ -23,7 +27,17 @@ def stock_market():
         condition = response.json()['finance']['result'] is None
         return PokeReturnValue(is_done=condition, xcom_value=url)
     
-    is_api_available()
+
+    get_stock_prices = PythonOperator(
+        task_id = 'get_stock_prices',
+        python_callable=_get_stock_prices,
+        op_kwargs={
+            'url': '{{ task_instance.xcom_pull(task_ids="is_api_available") }}', 
+            'symbol': SYMBOL
+            }
+    )
+
+    is_api_available() >> get_stock_prices
 
 
 
